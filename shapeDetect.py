65bbd1749a21dfbd7925ed7ee0521040035cb244
HUANGManutea <mhuang@insa-rennes.fr>
2016-02-27 18:53:45 +0100

    initial commit

---
+++ shapeDetect.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+import math
+import numpy as np
+import cv2
+
+#dictionary of all contours
+contours = {}
+#array of edges of polygon
+approx = []
+#scale of the text
+scale = 2
+#camera
+cap = cv2.VideoCapture(0)
+
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+#calculate angle
+def angle(pt1,pt2,pt0):
+    dx1 = pt1[0][0] - pt0[0][0]
+    dy1 = pt1[0][1] - pt0[0][1]
+    dx2 = pt2[0][0] - pt0[0][0]
+    dy2 = pt2[0][1] - pt0[0][1]
+    return float((dx1*dx2 + dy1*dy2))/math.sqrt(float((dx1*dx1 + dy1*dy1))*(dx2*dx2 + dy2*dy2) + 1e-10)
+
+while(cap.isOpened()):
+    #Capture frame-by-frame
+    ret, frame = cap.read()
+    if ret==True:
+        #grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #Canny
+        canny = cv2.Canny(frame,80,240,3)
+
+        #contours
+        canny2, contours, hierarchy = cv2.findContours(canny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        for i in range(0,len(contours)):
+            #approximate the contour with accuracy proportional to
+            #the contour perimeter
+            approx = cv2.approxPolyDP(contours[i],cv2.arcLength(contours[i],True)*0.02,True)
+
+            #Skip small or non-convex objects
+            if(abs(cv2.contourArea(contours[i]))<100 or not(cv2.isContourConvex(approx))):
+                continue
+
+            #triangle
+            if(len(approx) == 3):
+                x,y,w,h = cv2.boundingRect(contours[i])
+                cv2.putText(frame,'TRI',(x,y),cv2.FONT_HERSHEY_SIMPLEX,scale,(255,255,255),2,cv2.LINE_AA)
+            elif(len(approx)>=4 and len(approx)<=6):
+                #nb vertices of a polygonal curve
+                vtc = len(approx)
+                #get cos of all corners
+                cos = []
+                for j in range(2,vtc+1):
+                    cos.append(angle(approx[j%vtc],approx[j-2],approx[j-1]))
+                #sort ascending cos
+                cos.sort()
+                #get lowest and highest
+                mincos = cos[0]
+                maxcos = cos[-1]
+
+                #Use the degrees obtained above and the number of vertices
+                #to determine the shape of the contour
+                x,y,w,h = cv2.boundingRect(contours[i])
+                if(vtc==4):
+                    cv2.putText(frame,'RECT',(x,y),cv2.FONT_HERSHEY_SIMPLEX,scale,(255,255,255),2,cv2.LINE_AA)
+                elif(vtc==5):
+                    cv2.putText(frame,'PENTA',(x,y),cv2.FONT_HERSHEY_SIMPLEX,scale,(255,255,255),2,cv2.LINE_AA)
+                elif(vtc==6):
+                    cv2.putText(frame,'HEXA',(x,y),cv2.FONT_HERSHEY_SIMPLEX,scale,(255,255,255),2,cv2.LINE_AA)
+            else:
+                #detect and label circle
+                area = cv2.contourArea(contours[i])
+                x,y,w,h = cv2.boundingRect(contours[i])
+                radius = w/2
+                if(abs(1 - (float(w)/h))<=2 and abs(1-(area/(math.pi*radius*radius)))<=0.2):
+                    cv2.putText(frame,'CIRC',(x,y),cv2.FONT_HERSHEY_SIMPLEX,scale,(255,255,255),2,cv2.LINE_AA)
+
+        #Display the resulting frame
+        out.write(frame)
+        cv2.imshow('frame',frame)
+        cv2.imshow('canny',canny)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+#When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
